32ccb3ebdaba470a8e94d94324b7842c522cd5e3
Ananta Munuganti <Ananta Munuganti@DESKTOP-FCOL8H7>
2022-06-13 15:37:56 +0530

    commit-1 : initial commit

---
+++ src/main/java/com/twocoms/App.java
@@ -0,0 +1,13 @@
+package com.twocoms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
