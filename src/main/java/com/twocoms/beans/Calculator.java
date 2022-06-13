95cf59749ad376692588a16d145d1f491dd25baf
Ananta Munuganti <Ananta Munuganti@DESKTOP-FCOL8H7>
2022-06-13 16:09:03 +0530

    commit-1 : simple program of add , mul

---
+++ src/main/java/com/twocoms/beans/Calculator.java
@@ -0,0 +1,21 @@
+package com.twocoms.beans;
+
+public class Calculator {
+	
+public int add(int a, int b) {
+		
+		int c;
+		c= a+b;
+		return c;
+		
+	}
+
+	public int multiple(int a, int b) {
+		
+		int c;
+		c= a*b;
+		return c;
+	}
+
+
+}
