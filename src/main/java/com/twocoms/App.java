d93ab6036e12b9f352a87ad0635105f5b5841b6b
Ananta Munuganti <Ananta Munuganti@DESKTOP-FCOL8H7>
2022-06-13 15:46:26 +0530

    commit-2 : Added second line program

--- src/main/java/com/twocoms/App.java
+++ src/main/java/com/twocoms/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        System.out.print("Thank you for using java");
     }
 }
